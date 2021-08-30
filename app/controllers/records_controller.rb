f0d9285510c42365b8bb8c703c08c46052a7c032
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 21:33:18 +0900

    ストロングパラメーターの修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -16,7 +16,7 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record).permit(:date, :ashi, :pair, :entry_rate, :exit_rate, :pips, :entry_basis, :issue, :lose_reason, :lose_reason_text, :image).merge(user_id: current_user.id)
+    params.require(:record).permit(:date, :ashi_id, :pair_id, :entry_rate, :exit_rate, :pips, :entry_basis, :issue_id, :lose_reason_id, :lose_reason_text, :image).merge(user_id: current_user.id)
   end
 
 end
