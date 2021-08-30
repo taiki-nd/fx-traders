014970da1d1e7245b72dad3805b89c81a036399c
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 18:52:54 +0900

    バリデーションの設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,12 @@
 class Record < ApplicationRecord
+
+  with_options presence: true do
+    validates :date
+    validates :pips, format: {with: /\A[-]?[0-9]+(\.[0-9]+)?\z/}
+    validates :entry_basis
+    validates :lose_reason_text
+  end
+
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image 
 end
