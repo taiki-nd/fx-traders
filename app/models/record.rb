90d3d72ac7ef736cde7982339a3574018310b8f2
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 18:22:38 +0900

    アソシエーションの設定

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,4 @@
 class Record < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX
 
   validates :name, presence: true
+
+  has_many :records
 end
