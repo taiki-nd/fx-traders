96b1242d8a61194ed69197b22c5c067317c53142
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 20:51:27 +0900

    active hash (issue)

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,12 +6,14 @@ class Record < ApplicationRecord
     validates :pair_id
     validates :pips, format: {with: /\A[-]?[0-9]+(\.[0-9]+)?\z/}
     validates :entry_basis
+    validates :issue_id
     validates :lose_reason_text
   end
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :ashi_id
     validates :pair_id
+    validates :issue_id
   end
 
   belongs_to :user
