2743926d3c3edcc1c6228e89ce9e6849365058a0
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 18:27:13 +0900

    アクション記述

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,10 +5,18 @@ class RecordsController < ApplicationController
   end
 
   def create
+    @record = Record.new(record_params)
+    if item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
 
-  
+  def record_params
+    params.require(:record).permit(:date, :ashi, :pair, :entry_rate, :exit_rate, :pips, :entry_basis, :issue, :lose_reason, :lose_reason_text, :image).merge(user_id: current_user.id)
+  end
 
 end
