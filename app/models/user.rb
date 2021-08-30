0992dc777ea8f3ed2806cb0b916d14c29e76679a
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-30 15:18:47 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
