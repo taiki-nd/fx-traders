a889fb3630eec214fd236133f6380a2a7bb3731e
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-01 23:22:49 +0900

    詳細表示機能の実装（サーバーサイド）

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#top'
   get 'static_pages/about'
-  resources :records, only:[:index, :new, :create]
+  resources :records, only:[:index, :new, :create, :show]
 end
