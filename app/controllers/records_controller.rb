3cda42c180cb3f21265c18abe24201bab103a493
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-01 23:54:55 +0900

    修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,6 @@
 class RecordsController < ApplicationController
 
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @records = Record.includes(:user).order("created_at DESC").page(params[:page]).per(20)
