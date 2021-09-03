505d6a2bab0f0c5bc62002b60aa10915355c5d44
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-03 14:24:52 +0900

    バリデーションの設定

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,2 +1,21 @@
 class Rule < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :ashi_id
+    validates :pair_id
+    validates :issue_id
+    validates :overview
+    validates :overview_content
+    validates :entry
+    validates :exit
+    validates :summary
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :ashi_id
+    validates :pair_id
+    validates :issue_id
+  end
+
 end
