b5f65ee0d89242e5d243fbdcc9613f0bd04395ee
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-03 17:59:08 +0900

    取引手法投稿機能（サーバーサイド）

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -6,4 +6,19 @@ class RulesController < ApplicationController
     @rule = Rule.new
   end
 
+  def create
+    @rule = Rule.new(rule_params)
+    if @rule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rule_params
+    params.require(:rule).permit(:name, :ashi_id, :pair_id, :entry, :exit, :issue_id, :overview, :overview_content, :summary, :image).merge(user_id: current_user.id)
+  end
+
 end
