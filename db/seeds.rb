65a696b1e47e7312b3d2a8e45002ac1da5fa0da9
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-04 14:34:34 +0900

    seedでデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do 
+Record.create!(
+  date: '2020-12-12',
+  ashi_id: 3,
+  pair_id: 100,
+  pips: 22,
+  entry_basis:'www',
+  issue_id: 2,
+  lose_reason_id: 4,
+  lose_reason_text: 'www',
+  user_id: 2
+)
+end
