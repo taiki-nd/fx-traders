ae086723cc4eb104a4521bc0f9bdedf842215e80
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-04 18:16:08 +0900

    新データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,114 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do 
-Record.create!(
-  date: '2020-12-12',
-  ashi_id: 3,
-  pair_id: 108,
-  pips: 22,
-  entry_basis:'www',
-  issue_id: 2,
-  lose_reason_id: 4,
-  lose_reason_text: 'www',
-  user_id: 2
-)
-end
-
-7.times do 
+3.times do 
   Record.create!(
-    date: '2020-12-14',
+    date: '2020-12-20',
     ashi_id: 3,
-    pair_id: 108,
-    pips: 30,
+    pair_id: 51,
+    pips: 15,
     entry_basis:'www',
     issue_id: 2,
     lose_reason_id: 4,
     lose_reason_text: 'www',
     user_id: 2
   )
-  end
-
-  3.times do 
-    Record.create!(
-      date: '2020-12-12',
-      ashi_id: 3,
-      pair_id: 108,
-      pips: -8,
-      entry_basis:'www',
-      issue_id: 3,
-      lose_reason_id: 4,
-      lose_reason_text: 'www',
-      user_id: 2
-    )
-    end
-
-    6.times do 
-      Record.create!(
-        date: '2020-12-14',
-        ashi_id: 3,
-        pair_id: 108,
-        pips: -20,
-        entry_basis:'www',
-        issue_id: 3,
-        lose_reason_id: 4,
-        lose_reason_text: 'www',
-        user_id: 2
-      )
-      end
+end
 
 5.times do 
-Record.create!(
-date: '2020-12-17',
-ashi_id: 3,
-pair_id: 91,
-pips: 22,
-entry_basis:'www',
-issue_id: 2,
-lose_reason_id: 4,
-lose_reason_text: 'www',
-user_id: 2
-)
+  Record.create!(
+    date: '2020-12-20',
+    ashi_id: 3,
+    pair_id: 51,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
 end
 
-12.times do 
-Record.create!(
-date: '2020-12-18',
-ashi_id: 3,
-pair_id: 91,
-pips: 30,
-entry_basis:'www',
-issue_id: 2,
-lose_reason_id: 4,
-lose_reason_text: 'www',
-user_id: 2
-)
-end
-
-9.times do 
-Record.create!(
-date: '2020-12-17',
-ashi_id: 3,
-pair_id: 91,
-pips: -8,
-entry_basis:'www',
-issue_id: 3,
-lose_reason_id: 4,
-lose_reason_text: 'www',
-user_id: 2
-)
-end
-
-3.times do 
-Record.create!(
-  date: '2020-12-18',
-  ashi_id: 3,
-  pair_id: 91,
-  pips: -20,
-  entry_basis:'www',
-  issue_id: 3,
-  lose_reason_id: 4,
-  lose_reason_text: 'www',
-  user_id: 2
-)
-end
