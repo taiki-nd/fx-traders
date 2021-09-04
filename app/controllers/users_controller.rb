359af280639ce2f8cf4ecbdae56d7c0ebfef16e0
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-04 12:41:24 +0900

    アクションの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    @name = user.name
+    @records = user.records
+    @rules = user.rules
+  end
+
 end
