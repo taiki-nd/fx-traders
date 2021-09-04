f50a1a2c161c4facfda7bb634e7fbbc9eb3fb706
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-04 18:22:31 +0900

    グラフ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @records = user.records.order("created_at DESC").page(params[:page]).per(12)
-    @rules = user.rules.order("created_at DESC").page(params[:page]).per(8)
+    @user = User.find(params[:id])
+    @name = @user.name
+    @records = @user.records.order("created_at DESC").page(params[:page]).per(12)
+    @rules = @user.rules.order("created_at DESC").page(params[:page]).per(8)
+    @records_by_user = Record.where(user_id: @user.id)
+    @pair_cat = Record.group(:pair_id).pluck(:pair_id)
   end
 
 end
