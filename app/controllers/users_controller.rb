647db86618ee05f8903411a7467a713206e0874c
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-04 14:34:03 +0900

    ページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = user.name
-    @records = user.records
-    @rules = user.rules
+    @records = user.records.order("created_at DESC").page(params[:page]).per(12)
+    @rules = user.rules.order("created_at DESC").page(params[:page]).per(8)
   end
 
 end
