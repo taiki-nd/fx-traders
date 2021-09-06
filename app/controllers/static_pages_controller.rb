5d19a44fd2b36861d5cdcd0381d8f89e3ff1b53e
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-06 22:24:23 +0900

    pathの設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def top
+    @records = Record.limit(4).order("created_at DESC")
+    @rules = Rule.limit(4).order("created_at DESC")
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#top'
-  get 'static_pages/about'
+  get "about",to: 'static_pages#about'
   resources :records
   resources :rules
   resources :users, only: [:show]
