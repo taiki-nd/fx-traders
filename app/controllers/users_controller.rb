03542a74d4dc750c7b1f63a203e8f3cc190c5585
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-06 13:57:21 +0900

    グラフ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,12 @@ class UsersController < ApplicationController
     @rules = @user.rules.order("created_at DESC").page(params[:page]).per(8)
     @records_by_user = Record.where(user_id: @user.id)
     @pair_cat = @records_by_user.group(:pair_id).pluck(:pair_id)
+
+    @all_pips = Record.where('pips not like?', '-%').pluck(:pips)
+    @all_pips_ = Record.where('pips like?', '-%').pluck(:pips)
+    @all_pf = @all_pips.sum/@all_pips_.sum.abs
+
+
   end
 
 end
