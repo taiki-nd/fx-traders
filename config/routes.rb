ec667ae9d32db04118b32e0ca234c8c41e91e985
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-09 07:52:29 +0900

    アクション記述/record(入力)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "about",to: 'static_pages#about'
   resources :records do
     resources :comment_records
+    collection do
+      get 'search'
+    end
   end
   resources :rules do
     resources :comment_rules
