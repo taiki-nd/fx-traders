838c4f9eebe1f37b33ebdbaa462ea7e2fe6cbbdb
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-08 17:39:51 +0900

    新しいデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,3 +34,143 @@ end
   )
 end
 
+15.times do 
+  Record.create!(
+    date: '2020-12-20',
+    ashi_id: 3,
+    pair_id: 108,
+    pips: 15,
+    entry_basis:'www',
+    issue_id: 2,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+12.times do 
+  Record.create!(
+    date: '2020-12-20',
+    ashi_id: 3,
+    pair_id: 108,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+15.times do 
+  Record.create!(
+    date: '2021-09-01',
+    ashi_id: 3,
+    pair_id: 108,
+    pips: 15,
+    entry_basis:'www',
+    issue_id: 2,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+12.times do 
+  Record.create!(
+    date: '2021-09-01',
+    ashi_id: 3,
+    pair_id: 108,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+3.times do 
+  Record.create!(
+    date: '2020-11-20',
+    ashi_id: 3,
+    pair_id: 91,
+    pips: 15,
+    entry_basis:'www',
+    issue_id: 2,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+5.times do 
+  Record.create!(
+    date: '2020-11-21',
+    ashi_id: 3,
+    pair_id: 91,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+15.times do 
+  Record.create!(
+    date: '2021-08-29',
+    ashi_id: 3,
+    pair_id: 43,
+    pips: 15,
+    entry_basis:'www',
+    issue_id: 2,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+12.times do 
+  Record.create!(
+    date: '2020-08-30',
+    ashi_id: 3,
+    pair_id: 43,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+15.times do 
+  Record.create!(
+    date: '2021-08-31',
+    ashi_id: 3,
+    pair_id: 43,
+    pips: 15,
+    entry_basis:'www',
+    issue_id: 2,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
+12.times do 
+  Record.create!(
+    date: '2021-08-31',
+    ashi_id: 3,
+    pair_id: 43,
+    pips: -12,
+    entry_basis:'www',
+    issue_id: 3,
+    lose_reason_id: 4,
+    lose_reason_text: 'www',
+    user_id: 2
+  )
+end
+
