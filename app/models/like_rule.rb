f34be0d294e2eea36098d81681ed4d9cefeafdef
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-09 22:20:00 +0900

    アソシエーション（rule）

--- app/models/like_rule.rb
+++ app/models/like_rule.rb
@@ -1,2 +1,6 @@
 class LikeRule < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :rules
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :records
   has_many :rules
   has_many :like_records
+  has_many :like_rules
 
   def liked_by?(record_id)
     like_records.where(record_id: record_id).exists?
