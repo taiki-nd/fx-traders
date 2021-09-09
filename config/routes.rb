c0f3577b09e95f6f7706fb4d3de22f72329e9364
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-09 09:28:39 +0900

    アクション記述（rule）

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   end
   resources :rules do
     resources :comment_rules
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: [:edit, :update, :show]
 end
