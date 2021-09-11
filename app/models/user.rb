237df6baa4752afcfffe068cac348f296bbe41bb
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-11 20:45:46 +0900

    フォロー機能のメソッドの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,17 +14,32 @@ class User < ApplicationRecord
   has_many :like_records
   has_many :like_rules
 
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
-
   def liked_by?(record_id)
     like_records.where(record_id: record_id).exists?
   end
 
   def liked__by?(rule_id)
     like_rules.where(rule_id: rule_id).exists?
+  end
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
   end
 
 end
