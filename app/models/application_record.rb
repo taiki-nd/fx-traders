f3f5d62b26ad5ce106ad149c6eb585ee6043d31b
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-23 00:43:28 +0900

    次のページ、前のページの実装の中止
    絞り込まれている状態で行うことができない。

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
+=begin
   def next_record
     Record.where("id < ?", self.id).order("id DESC").first
   end
@@ -17,4 +17,17 @@ class ApplicationRecord < ActiveRecord::Base
     Rule.where("id > ?", self.id).order("id ASC").first
   end
 
+
+  def next_record_user
+    @record = Record.find(id)
+    @user = @record.user
+    Record.where(user_id: @user.id).where("id < ?", self.id).order("id DESC").first
+  end
+
+  def previous_record_user
+    @record = Record.find(id)
+    @user = @record.user
+    Record.where(user_id: @user.id).where("id > ?", self.id).order("id ASC").first
+  end
+=end
 end
