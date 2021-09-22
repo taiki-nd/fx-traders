1f928e5917bc0bb49ad7a2496d81b4083f679aec
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-22 14:44:49 +0900

    ページ遷移の実装（rule）

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,4 +9,12 @@ class ApplicationRecord < ActiveRecord::Base
     Record.where("id > ?", self.id).order("id ASC").first
   end
 
+  def next_rule
+    Rule.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def previous_rule
+    Rule.where("id > ?", self.id).order("id ASC").first
+  end
+
 end
