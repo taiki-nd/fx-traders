76faad96e0de3b8742e76096888de5f2f16f5ab2
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-22 14:21:03 +0900

    ページ遷移の実装（record）
    次のページ、前のページ

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def next_record
+    Record.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def previous_record
+    Record.where("id > ?", self.id).order("id ASC").first
+  end
+
 end
