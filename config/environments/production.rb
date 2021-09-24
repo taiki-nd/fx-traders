6cb157c8acc1e78d330650ebd4cb29965557d286
Taiki Noda <taiki.nd.real@gmail.com>
2021-09-24 19:57:13 +0900

    メール認証（heroku）

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,27 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'https://fx-traders.herokuapp.com/'}
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: ENV["INFO_MAIL_AD"],
+    password: ENV["INFO_MAIL_PASS"],
+    authentication: :login,
+    openssl_verify_mode: 'none',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
