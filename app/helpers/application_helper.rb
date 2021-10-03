9a481c25c1dc8901c76962491289a8515cf93cc1
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-03 11:20:07 +0900

    ogpの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,13 +20,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('logo.png'),
+        image: image_url('trading_note_.jpg'),
         local: 'ja-JP',
       },
       twitter: {
         card: 'summary_large_image',
         site: '@FX_Traders_',
-        image: image_url('sample.jpg')
+        image: image_url('trading_note_.jpg'),
       }
     }
   end
