b8ac2472bcfde4aab65d6b32cd4dbac4adecd271
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-17 14:09:42 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
       description: '「自分の取引記録が最高の取引教材になるその時まで」。fxの取引の記録を残して、自身の取引精度を上げていきましょう！自身の手法を公開してフィードバックをもらいましょう！',
       keywords: 'fx,取引記録,投資',
       canonical: request.original_url,
-      separator: '-',
+      separator: '|',
       icon: [
         { href: image_url('logo.png') },
         { href: image_url('logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
