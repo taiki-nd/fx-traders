b6d907a9cfa761d3da3902047638c9398402f4e3
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-17 10:53:54 +0900

    topページに広告表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def top
     @records = Record.limit(4).order("created_at DESC")
     @rules = Rule.limit(4).order("created_at DESC")
+    @ad_2 = Advertisement.where(ad_rate_id: 2).order("RAND()").first
+    @ad_5 = Advertisement.where(ad_rate_id: 5).order("RAND()").first
   end
 
   def privacy_policy
